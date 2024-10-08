21a41e159347f5197bed4f4da7ded5d67f92a7d8
Adam French [sc21a2f] <sc21a2f@uol-pc-055426.leeds.ac.uk>
2024-10-08 17:01:55 +0100

    added vertex neighbors

--- graph.c
+++ graph.c
@@ -32,16 +32,32 @@ void free_graph(Graph* g) {
 void print_graph(Graph* g) {
 	int n = g->num_vtx;
 	for (int i=0; i<n; i++) {
-		for (int j=0; j<n; j++) {
-			printf("%2d ", g->adj_mat[i][j]);
-		}
-		printf("\n");
+		print_array(g->adj_mat[i], n);
 	}
 }
 
 // Adds an edge connecting i->j with capacity c
-void add_capacity(Graph *g, int i, int j, int c) {
-	g->adj_mat[i][j] = c;
+void add_edge(Graph *g, int u, int v, int c) {
+	g->adj_mat[u][v] = c;
+}
+
+// Get the neighbors of a vertex
+int* vertex_neighbors(Graph* g, int u, int* len) {
+	int n = g->num_vtx;
+
+	int* nbrs = calloc(n, sizeof(int));
+	int nbr_count = 0;
+
+	for (int i=0; i<n; i++) {
+		if (g->adj_mat[u][i] > 0) {
+			nbrs[nbr_count++] = i;
+		}
+	}
+
+	int* realloc_nbrs = realloc(nbrs, sizeof(int)*n);
+	nbrs = realloc_nbrs;
+	*len = nbr_count;
+	return nbrs;
 }
 
 //Randomize the capacities of a graph
@@ -53,6 +69,54 @@ void randomize_graph(Graph* g, int max) {
 		for (int j=0; j<n; j++) {
 			g->adj_mat[i][j] = rand() % max;
 		}
+		// Make sure diagonal is 0
 		g->adj_mat[i][i] = 0;
+	}
+}
+
+
+//Find a path from vertex u to vertex v
+int* dfs_find_path(Graph* g, int u, int v) {
+	int n = g->num_vtx;
+	// Make a list for checking if we have looked at the vertex
+	int* chk_vtx = calloc(n, sizeof(int)); 
+	// Make a stack for what verticies we should explore first (-1 = not a vtx)
+	int* stk = calloc(n, sizeof(int)); 
+	fill_array(stk, n, -1);
+
+	
+	
+	print_array(stk,n);
+
+	free(stk);
+	free(chk_vtx);
+	return (int*)NULL;
+}
+
+int* bfs_find_path(Graph* g, int u, int v) {
+	int n = g->num_vtx;
+	// Make a graph for checking if we have looked at a value or not. 1=seen 0=notseen
+	int* chk_vtx = calloc(n, sizeof(int)); 
+	// Make a queue for what we should explore
+	int* que = calloc(n, sizeof(int)); 
+	fill_array(que, n, -1);
+
+	free(que);
+	free(chk_vtx);
+	return (int*)NULL;
+}
+
+// Helper function for printing an array
+void print_array(int* arr, int n) {
+	for (int i=0; i<n; i++) {
+		printf("%2d ", arr[i]);
+	}
+	printf("\n");
+}
+
+// Helper function for initializing an array
+void fill_array(int* arr, int n, int v) {
+	for (int i=0; i<n; i++) {
+		arr[i] = v;
 	}
 }
--- graph.h
+++ graph.h
@@ -7,8 +7,18 @@ typedef struct {
 
 Graph make_graph(int n);
 void free_graph(Graph* g);
+
 void print_graph(Graph* g);
-void add_capacity(Graph* g, int i, int j, int c);
+
+void add_edge(Graph* g, int u, int v, int c);
+int* vertex_neighbors(Graph* g, int u, int* len);
+
 void randomize_graph(Graph* g, int max);
+
+int* dfs_find_path(Graph* g, int u, int v);
+int* bfs_find_path(Graph* g, int u, int v);
+
+void print_array(int* arr, int n);
+void fill_array(int* arr, int n, int v);
 
 #endif
