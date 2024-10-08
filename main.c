c22df1db524f48743179c59b1838ab5b37a8ba22
Adam French [sc21a2f] <sc21a2f@uol-pc-055426.leeds.ac.uk>
2024-10-08 17:50:38 +0100

    debug test

--- main.c
+++ main.c
@@ -3,10 +3,28 @@
 
 #include "graph.h"
 
-int main() {
-	Graph g = make_graph(10);
-	add_capacity(&g, 2, 2, 8);
+int main(int argc, char* argv[]) {
+	int n = 10;
+
+	if (argc > 1) {
+		n=atoi(argv[1]);
+	}
+
+	Graph g = make_graph(n);
+	randomize_graph(&g, n);
 	print_graph(&g);
+	printf("\n");
+
+	int len;
+	int* arr = vertex_neighbors(&g, rand()%n, &len);
+
+	int p_len;
+	int u = rand()%n;
+	int v = rand()%n;
+	int* path = dfs_find_path(&g, u, v, &p_len);
+	printf("u:%2d, v:%2d\n", u, v);
+	print_array(path, p_len);
+
 	free_graph(&g);
 	return 0;
 }
