6a56cce3b8703d5985cf7863c1deb61c3fbb62fb
Adam French [sc21a2f] <sc21a2f@uol-pc-055426.leeds.ac.uk>
2024-10-08 17:50:24 +0100

    accepts arguments now

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 BUILD_DIR=.build
 
-gcc main.c graph.c -o $BUILD_DIR/main.out && $BUILD_DIR/main.out
+gcc main.c graph.c -o $BUILD_DIR/main.out
+
+if [ $? -eq 0 ]; then
+	$BUILD_DIR/main.out "$@"
+else
+	echo "Compilation failed"
+fi
