3ecb65c9b29bcf9a038a1685a644539f32ace8c9
Adam French <adam.a.french@outlook.com>
2024-10-14 09:32:15 +0100

    added header for list including queue and stack

---
+++ list.h
@@ -0,0 +1,27 @@
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct {
+  int val;
+  struct Node* next, prev;
+} Node;
+
+typedef struct {
+  Node* head, tail;
+  int n;
+} List;
+
+int getIndex(List* l, int i);
+
+int getFront(List* l);
+int getRear(List* l);
+int isEmpty(List* l);
+
+int enqueue(List* l, int i);
+int dequeue(List* l);
+
+void push(List* l, int i);
+int pop(List* l);
+int peek(List* l);
+
+#endif
